fe0bfc37d7669dfc16378eaf91b38b3d2817e7b7
billywithbelly <billychen1216@gmail.com>
2018-06-07 20:12:08 +0800

    remove cyclebehavior

--- src/main/java/behaviour/CheckStateBehavior.java
+++ src/main/java/behaviour/CheckStateBehavior.java
@@ -2,11 +2,12 @@ package behaviour;
 
 import agents.Taxi;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.SimpleBehaviour;
 
 /**
  * Created by eduardosalazar1 on 6/13/16.
  */
-public class CheckStateBehavior extends CyclicBehaviour {
+public class CheckStateBehavior extends SimpleBehaviour {
     private final Taxi agent;
     public CheckStateBehavior(Taxi taxi){
         agent = taxi;
@@ -16,5 +17,10 @@ public class CheckStateBehavior extends CyclicBehaviour {
     public void action() {
         // Check call taxi self check
         agent.checkStatus();
+    }
+
+    @Override
+    public boolean done() {
+        return false;
     }
 }
